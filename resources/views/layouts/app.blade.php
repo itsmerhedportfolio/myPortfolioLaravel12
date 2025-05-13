a20f5fa850038deb2db284ca423bdd1e78ec8de2
Rhedaligan8 <rhedaligan8@gmail.com>
2025-05-12 22:20:53 -0900

    starter v1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Laravel</title>
+    <title>Rhed </title>
+
+    <link rel="icon" href="{{ asset('build/assets/images/image.png') }}" type="image/png">
+
 </head>
 <body>
-    <h1>Oops, you are in the welcom blade file ! :) </h1>
+
+@include('components.navigationbar')
 </body>
 </html>
